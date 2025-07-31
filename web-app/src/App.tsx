fb33e5e6189e9764c834e472384d09ce650c2534
HironoriUchibori <hironori.uchibori.298@gmail.com>
2025-07-31 22:24:37 +0900

    minor change

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import { Header } from './components/Header/Header'
-import { Footer } from './components/Footer/Footer'
 import Layout from './components/Layout/Layout'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Layout>
